b90913c6dbc725e1d35e6e5e312dc9bb34adf4cf
Mickey1216 <383449174@qq.com>
2023-08-09 19:32:12 +0800

    增加获取复习单词接口

--- src/word/word.controller.ts
+++ src/word/word.controller.ts
@@ -15,28 +15,33 @@ import { UpdateWordDto } from './dto/update-word.dto';
 export class WordController {
   constructor(private readonly wordService: WordService) {}
 
-  @Post('add')
+  // 添加单词
+  @Post('addWord')
   create(@Body() createWordDto: CreateWordDto) {
     return this.wordService.create(createWordDto);
   }
 
-  @Post('getAll')
-  findAll(@Body() body: any) {
-    return this.wordService.findAll(body.belonging);
+  // 获取用户所有的单词
+  @Post('getWordsList')
+  getAll(@Body() body: any) {
+    return this.wordService.getAll(body.belonging);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.wordService.findOne(+id);
+  // 获取单词的总个数
+  @Post('getWordsCount')
+  getWordsCount(@Body() body: any) {
+    return this.wordService.getWordsCount(body.belonging);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateWordDto: UpdateWordDto) {
-    return this.wordService.update(+id, updateWordDto);
+  // 获取复习单词的总个数
+  @Post('getReviewWordsCount')
+  getReviewWordsCount(@Body() body: any) {
+    return this.wordService.getReviewWordsCount(body);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.wordService.remove(+id);
+  // 获取复习单词
+  @Post('getReviewWords')
+  getReviewWords(@Body() body: any) {
+    return this.wordService.getReviewWords(body);
   }
 }
--- src/word/word.service.ts
+++ src/word/word.service.ts
@@ -11,28 +11,84 @@ export class WordService {
     @InjectRepository(Word) private readonly wordRepository: Repository<Word>,
   ) {}
 
+  // 添加单词
   create(createWordDto: CreateWordDto) {
     return this.wordRepository.save(createWordDto);
   }
 
-  async findAll(belonging: string) {
+  // 获取用户所有的单词
+  async getAll(belonging: string) {
     const wordsList = await this.wordRepository.find({ where: { belonging } });
     return {
       code: 200,
       message: '获取成功',
-      wordsList,
+      data: wordsList,
     };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} word`;
+  // 获取单词的总个数
+  async getWordsCount(belonging: string) {
+    const wordsCount = await this.wordRepository.countBy({
+      belonging,
+    });
+    return {
+      code: 200,
+      message: '获取成功',
+      data: wordsCount,
+    };
   }
 
-  update(id: number, updateWordDto: UpdateWordDto) {
-    return `This action updates a #${id} word`;
+  // 获取复习单词的总个数
+  async getReviewWordsCount(body: any) {
+    const { belonging, type } = body;
+    let reviewWordsCount = 0;
+    // 如果没有选择单词类型或者选择了两个单词类型，则获取所有类型的单词
+    if (type.length === 0 || type.length === 2) {
+      reviewWordsCount = await this.wordRepository.countBy({
+        belonging,
+      });
+    } else {
+      // 根据选择的单词类型获取单词
+      reviewWordsCount = await this.wordRepository.countBy({
+        belonging,
+        mark: parseInt(type[0]),
+      });
+    }
+    return {
+      code: 200,
+      message: '获取成功',
+      data: reviewWordsCount,
+    };
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} word`;
+  // 获取复习单词
+  async getReviewWords(body: any) {
+    const { belonging, type, count, order } = body;
+    let reviewWords = [];
+    // 顺序获取单词
+    if (type.length === 0 || type.length === 2) {
+      // 如果没有选择单词类型或者选择了两个单词类型，则获取所有类型的单词
+      reviewWords = await this.wordRepository.find({
+        where: { belonging },
+        take: parseInt(count),
+      });
+    } else {
+      // 根据选择的单词类型获取单词
+      reviewWords = await this.wordRepository.find({
+        where: { belonging, mark: parseInt(type[0]) },
+        take: parseInt(count),
+      });
+    }
+
+    if (order === 'random') {
+      // 随机获取单词
+      reviewWords.sort(() => Math.random() - 0.5);
+    }
+
+    return {
+      code: 200,
+      message: '获取成功',
+      data: reviewWords,
+    };
   }
 }
